acd495f794d2f0afc4a004abaf1b3c1f32ff1f60
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-07-14 03:16:18 +0200

    Добавляет маршрут для обработки ситуаций, когда пользователь обращается к несуществующей странице

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,6 +5,7 @@ import MyListPage from '../../pages/mylist-page/mylist-page';
 import FilmPage from '../../pages/film-page/film-page';
 import AddReviewPage from '../../pages/add-review-page/add-review-page';
 import PlayerPage from '../../pages/player-page/player-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import { AppRoute } from '../../const';
 
 type AppProps = {
@@ -46,6 +47,10 @@ function App({ title, genre, date }: AppProps): JSX.Element {
         <Route
           path={AppRoute.Player}
           element={<PlayerPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
         />
       </Routes>
     </BrowserRouter>
