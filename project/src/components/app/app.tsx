a03d5cdfda9536f92ce01d73203796d482e4e352
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-07-14 03:43:42 +0200

    Создает компонент для описания приватных маршрутов и обновляет маршрут для страницы «MyList»

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -6,7 +6,8 @@ import FilmPage from '../../pages/film-page/film-page';
 import AddReviewPage from '../../pages/add-review-page/add-review-page';
 import PlayerPage from '../../pages/player-page/player-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
-import { AppRoute } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   title: string,
@@ -34,7 +35,13 @@ function App({ title, genre, date }: AppProps): JSX.Element {
         />
         <Route
           path={AppRoute.MyList}
-          element={<MyListPage />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyListPage />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Film}
