83e03580e0139084b8ef6ce5d37efa11298a5d33
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-07-20 01:14:32 +0200

    Добавляет в стейт компонента списка фильмов активную карточку

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from 'react';
 import { Film } from '../../types/film';
 import SmallFilmCard from '../../components/small-film-card/small-film-card';
 
@@ -7,9 +8,30 @@ type FilmsListProps = {
 
 function FilmsList({ films }: FilmsListProps): JSX.Element {
 
+  const [activeId, setActiveId] = useState({
+    id: '',
+  });
+
+  const mouseOverHandler = (evt: MouseEvent<HTMLDivElement>) => {
+    evt.preventDefault();
+    const target = evt.target as HTMLDivElement;
+    setActiveId({
+      ...activeId,
+      id: target.id,
+    });
+  };
+
   return (
-    <div className="catalog__films-list">
-      {films.map((film) => <SmallFilmCard film={film} key={film.name} />)}
+    <div
+      className="catalog__films-list"
+      onMouseOver={mouseOverHandler}
+    >
+      {films.map((film) => (
+        <SmallFilmCard
+          film={film}
+          key={film.name}
+        />
+      ))}
     </div>
   );
 }
